aa1779b9d012a20b386dd519f3e526b28fa8fcd0
changmin Lee / 이창민 <92606611+EeeasyCode@users.noreply.github.com>
2022-01-25 14:51:34 +0900

    winston 프로젝트에 적용

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,6 +10,9 @@ const id = document.querySelector("#id"),
 loginBtn.addEventListener("click", login);
 
 function login(){
+    if (!id.value) return alert("아이디를 입력해주십시오.");
+    if (!password.value) return alert("비밀번호를 입력해주십시오.");
+ 
     const req = {
         id: id.value, 
         psword: psword.value,
@@ -27,6 +30,7 @@ function login(){
             if (res.success){
                 location.href="/";
             }else{
+                if (res.err) return alert(res.err);
                 alert(res.msg);
             }
         })
--- app/src/routes/home/home.control.js
+++ app/src/routes/home/home.control.js
@@ -1,33 +1,53 @@
 "use strict";
 
 const User = require("../../models/Users");
-const UserStorage = require("../../models/UserStorage");
+const logger = require("../../config/logger");
 
-
+//get 메서드
 const output = {
     root: (req, res) => {
-        res.render("home/index")
+        logger.info(`GET / 200 홈 화면으로 이동`);
+        res.render("home/index");
     },
 
     login: (req, res) => {
+        logger.info(`GET / 200 로그인 화면으로 이동`);
         res.render("home/login");
     },
 
     register: (req, res) => {
+        logger.info(`GET / 200 회원가입 화면으로 이동` );
         res.render("home/register");
     }
         
 };
 
+//post 메서드
 const process = {
     login: async (req, res) => {  
         const user = new User(req.body);
-        const response = await user.login();    
+        const response = await user.login();
+        if (response.err)
+            logger.error(
+                `POST /login 200 Response: "Success: ${response.success}, ${response.err}"` 
+            );
+        else
+            logger.info(
+                `POST /login 200 Response: "Success: ${response.success}, msg: ${response.msg}"`
+            );    
         return res.json(response);
     },
     register: async (req, res) => {
         const user = new User(req.body);
-        const response = await user.register();   
+        const response = await user.register();
+        if (response.err)
+            logger.error(
+                `POST /login 200 Response: "Success: ${response.success}, ${response.err}"` 
+            );
+        else         
+            logger.info(
+                `POST /register 200 Response: "Success: ${response.success}, msg: ${response.msg}"`
+            );    
         return res.json(response);      
     },
 };
